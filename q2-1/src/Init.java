b0a4810efb2c2997a888cf5f85838f98e64897d5
SinLapis <stradust0001@gmail.com>
2018-09-25 11:05:04 +0800

    first commit

---
+++ q2-1/src/Init.java
@@ -0,0 +1,11 @@
+public class Init {
+    private int x;
+    private char y;
+    public static void main(String[] args) {
+        Init i = new Init();
+        System.out.print("x:");
+        System.out.println(i.x);
+        System.out.print("y:");
+        System.out.println(i.y);
+    }
+}
